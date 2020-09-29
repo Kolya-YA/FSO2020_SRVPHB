a1fcb832812183b68caa10f74e142d04b95121cc
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-09-29 18:25:43 +0200

    add fronend & deploy scripts

---
+++ build/precache-manifest.15958e7efe0dfae7b0aa750beb859642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e02ac8dcf11c1a02c03b73679f4d1c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0717960cc57676aef7c",
+    "url": "/static/js/2.f75b3e83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f75b3e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63c3f58a89b44db5ab6",
+    "url": "/static/js/main.7d28ecff.chunk.js"
+  },
+  {
+    "revision": "27262c86774d862b7b21",
+    "url": "/static/js/runtime-main.1bfbb25b.js"
+  }
+]);
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
+
 app.use(morgan('tiny', {
   skip: req => req.method === 'POST'
 }))
